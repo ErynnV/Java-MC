af1ab28665ba83c042a20d21f97858840718b560
Erynn <vandreerynn@gmail.com>
2025-01-13 17:40:38 +0100

    Init

---
+++ java/fr/tyrolium/maxime/atelierstage/util/handlers/IHasModel.java
@@ -0,0 +1,6 @@
+package fr.tyrolium.maxime.atelierstage.util.handlers;
+
+public interface IHasModel {
+
+    public void registerModels();
+}
---
+++ java/fr/tyrolium/maxime/atelierstage/util/handlers/RegistryHandler.java
@@ -0,0 +1,38 @@
+package fr.tyrolium.maxime.atelierstage.util.handlers;
+
+ import net.minecraft.block.Block;
+ import net.minecraft.item.Item;
+ import net.minecraftforge.client.event.ModelRegistryEvent;
+ import net.minecraftforge.event.RegistryEvent;
+ import  net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@EventBusSubscriber
+public class RegistryHandler {
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event){
+        event.getRegistry().registerAll();
+    }
+
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event){
+        event.getRegistry().registerAll();
+    }
+
+    @SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event){
+
+        for (Item item : /*nos items*/){
+            if (item instanceof IHasModel){
+                ((IHasModel)item).registerModels();
+            }
+        }
+
+        for (Block block : /*nos blocks*/){
+            if (block instanceof IHasModel){
+                ((IHasModel)block).registerModels();
+            }
+        }
+    }
+}
